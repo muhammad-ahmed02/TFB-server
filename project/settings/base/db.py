a0e386b4a84188692cd377014e033f64a00cef95
MAhmed13 <mahmed743454@gmail.com>
2022-05-29 21:58:33 +0500

    deployment settings

--- project/settings/base/db.py
+++ project/settings/base/db.py
@@ -3,20 +3,20 @@ import sys
 # Database
 # https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': '{{ project_name }}',
-#         'USER': '{{ project_name }}',
-#         'PASSWORD': '{{ secret_key|slugify }}',
-#         'HOST': 'db',
-#         'CONN_MAX_AGE': 900
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd292op9ngu1r3m',
+        'USER': 'dyopwxyfvvshru',
+        'PASSWORD': '4fdfdf5b8a38e3827d9f0bb794cb1d415a0c015685f16fee534c19dfa87f24b6',
+        'HOST': 'ec2-44-196-174-238.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'mydatabase',
+#     }
+# }

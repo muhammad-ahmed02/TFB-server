ff3c8cc297adb5e3f3f6a94449c8d8f711a08239
umer2001 <umer2001.uf@gmail.com>
2022-08-26 17:29:49 +0500

    make: db dynamic a/c to env

--- project/settings/base/db.py
+++ project/settings/base/db.py
@@ -1,6 +1,9 @@
 # Database
 # https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#databases
 
+import os
+
+
 local = False
 
 if local:
@@ -14,10 +17,10 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'd292op9ngu1r3m',
-            'USER': 'dyopwxyfvvshru',
-            'PASSWORD': '4fdfdf5b8a38e3827d9f0bb794cb1d415a0c015685f16fee534c19dfa87f24b6',
-            'HOST': 'ec2-44-196-174-238.compute-1.amazonaws.com',
-            'PORT': '5432',
+            'NAME': os.environ.get('DB_NAME', 'd292op9ngu1r3m'),
+            'USER': os.environ.get('DB_USER_NAME', 'dyopwxyfvvshru'),
+            'PASSWORD': os.environ.get('DB_PASSWORD', '4fdfdf5b8a38e3827d9f0bb794cb1d415a0c015685f16fee534c19dfa87f24b6'),
+            'HOST': os.environ.get('DB_HOST', 'ec2-44-196-174-238.compute-1.amazonaws.com'),
+            'PORT': os.environ.get('DB_PORT', '5432'),
         }
     }

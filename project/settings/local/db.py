0ba6ebeac9e0432e48acde4f4aaf1416fcad454d
Ahmed <shiekhahmed135@gmail.com>
2023-09-16 17:32:53 +0500

    db

--- project/settings/local/db.py
+++ project/settings/local/db.py
@@ -1,9 +1,9 @@
 # Database
 # https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'mydatabase',
+#     }
+# }

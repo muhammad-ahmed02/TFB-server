c65d85188ad54d89c81b148ace85a6c1bb7c4b8b
Ahmed <shiekhahmed135@gmail.com>
2023-09-17 03:27:36 +0500

    postgres db url added in settings

--- project/settings/base/db.py
+++ project/settings/base/db.py
@@ -2,7 +2,7 @@
 # https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#databases
 
 import os
-
+import dj_database_url
 
 local = False
 
@@ -15,12 +15,15 @@ if local:
     }
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ.get('DB_NAME', 'd79htkqnf866c2'),
-            'USER': os.environ.get('DB_USER_NAME', 'zngfvgpojnrsti'),
-            'PASSWORD': os.environ.get('DB_PASSWORD', '201bd84ecfbf9cb934c6a17be1dd530fbd8526532e3777c5fb4da324d7a7571a'),
-            'HOST': os.environ.get('DB_HOST', 'ec2-34-236-103-63.compute-1.amazonaws.com'),
-            'PORT': os.environ.get('DB_PORT', '5432'),
-        }
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #         'NAME': os.environ.get('DB_NAME', 'd79htkqnf866c2'),
+    #         'USER': os.environ.get('DB_USER_NAME', 'zngfvgpojnrsti'),
+    #         'PASSWORD': os.environ.get('DB_PASSWORD', '201bd84ecfbf9cb934c6a17be1dd530fbd8526532e3777c5fb4da324d7a7571a'),
+    #         'HOST': os.environ.get('DB_HOST', 'ec2-34-236-103-63.compute-1.amazonaws.com'),
+    #         'PORT': os.environ.get('DB_PORT', '5432'),
+    #     }
+    # }

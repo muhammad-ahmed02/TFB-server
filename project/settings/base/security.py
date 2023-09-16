8d69743de7d8cc2e7aa424a8be596cdc8e7fc2ba
Ahmed <shiekhahmed135@gmail.com>
2023-09-16 17:51:27 +0500

    heroku-22

--- project/settings/base/security.py
+++ project/settings/base/security.py
@@ -1,8 +1,7 @@
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '{{ secret_key }}'
 
-ALLOWED_HOSTS = ['tfb-server.herokuapp.com',
-                 'tfb-server-dev.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['tfb-server.herokuapp.com', 'localhost', '127.0.0.1']
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
